13ccaf02db1f2c5b574120760c51d57f8ed57d15
Pedro Vilas Bôas <phvb20@gmail.com>
2024-05-22 00:33:15 -0300

    adicionado diversos ajustes no cadastro e login | feito header de cliente e funcionario | feito pagina de cardapio de cliente | condigo funcional

--- client/src/pages/funcionario/PedidosEmAberto.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const headerContainer = document.getElementById('header-container');
-    fetch('../../utils/HeaderFuncionario.html')
-        .then(response => response.text())
-        .then(data => {
-            headerContainer.innerHTML = data;
-        });
-});
--- client/src/pages/funcionario/PedidosEmAberto/PedidosEmAberto.js
+++ client/src/pages/funcionario/PedidosEmAberto/PedidosEmAberto.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const headerContainer = document.getElementById('header-container');
-    fetch('../../utils/HeaderCliente.html')
+    fetch('../../../utils/HeaderFuncionario.html')
         .then(response => response.text())
         .then(data => {
             headerContainer.innerHTML = data;
